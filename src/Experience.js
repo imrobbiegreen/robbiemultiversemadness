c627d1e6356fec96d0281659f072871ff82c2e0b
imrobbiegreen <66812611+imrobbiegreen@users.noreply.github.com>
2022-11-28 13:59:41 -0600

    Initial commit

---
+++ src/Experience.js
@@ -0,0 +1,26 @@
+import { Physics, Debug } from '@react-three/rapier'
+import useGame from './stores/useGame.js'
+import Lights from './Lights.js'
+import { Level } from './Level.js'
+import Player from './Player.js'
+import Effects from './Effects.js'
+
+export default function Experience()
+{
+    const blocksCount = useGame((state) => state.blocksCount)
+    const blocksSeed = useGame(state => state.blocksSeed)
+
+    return <>
+
+        <color args={ [ '#252731' ] } attach="background" />
+
+        <Physics>
+            {/* <Debug /> */}
+            <Lights />
+            <Level count={ blocksCount } seed={ blocksSeed } />
+            <Player />
+        </Physics>
+
+        <Effects />
+    </>
+}
