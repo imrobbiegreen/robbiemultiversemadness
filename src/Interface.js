f28f81393f6338e261f956394011e1bbd2c2de90
imrobbiegreen <66812611+imrobbiegreen@users.noreply.github.com>
2023-07-18 13:57:43 -0500

    Update Interface.js

--- src/Interface.js
+++ src/Interface.js
@@ -1,69 +1,151 @@
-import { useKeyboardControls } from '@react-three/drei'
-import useGame from './stores/useGame.js'
-import { useEffect, useRef } from 'react'
-import { addEffect } from '@react-three/fiber'
+import { useState, useEffect, useRef } from 'react';
+import { useKeyboardControls } from '@react-three/drei';
+import useGame from './stores/useGame.js';
+import { addEffect } from '@react-three/fiber';
 
-export default function Interface()
-{
-    const time = useRef()
+export default function Interface() {
+  const time = useRef();
 
-    const restart = useGame((state) => state.restart)
-    const phase = useGame((state) => state.phase)
+  const restart = useGame((state) => state.restart);
+  const phase = useGame((state) => state.phase);
 
-    const forward = useKeyboardControls((state) => state.forward)
-    const backward = useKeyboardControls((state) => state.backward)
-    const leftward = useKeyboardControls((state) => state.leftward)
-    const rightward = useKeyboardControls((state) => state.rightward)
-    const jump = useKeyboardControls((state) => state.jump)
+  const [isMovingForward, setIsMovingForward] = useState(false);
+  const [isMovingBackward, setIsMovingBackward] = useState(false);
+  const [isMovingLeftward, setIsMovingLeftward] = useState(false);
+  const [isMovingRightward, setIsMovingRightward] = useState(false);
+  const [isJumping, setIsJumping] = useState(false);
 
-    useEffect(() =>
-    {
-        const unsubscribeEffect = addEffect(() =>
-        {
-            const state = useGame.getState()
+  const forward = useKeyboardControls((state) => state.forward);
+  const backward = useKeyboardControls((state) => state.backward);
+  const leftward = useKeyboardControls((state) => state.leftward);
+  const rightward = useKeyboardControls((state) => state.rightward);
+  const jump = useKeyboardControls((state) => state.jump);
 
-            let elapsedTime = 0
+  const handleForwardClick = () => {
+    setIsMovingForward(true);
+  };
 
-            if(state.phase === 'playing')
-                elapsedTime = Date.now() - state.startTime
-            else if(state.phase === 'ended')
-                elapsedTime = state.endTime - state.startTime
+  const handleBackwardClick = () => {
+    setIsMovingBackward(true);
+  };
 
-            elapsedTime /= 1000
-            elapsedTime = elapsedTime.toFixed(2)
+  const handleLeftwardClick = () => {
+    setIsMovingLeftward(true);
+  };
 
-            if(time.current)
-                time.current.textContent = elapsedTime
-        })
+  const handleRightwardClick = () => {
+    setIsMovingRightward(true);
+  };
 
-        return () =>
-        {
-            unsubscribeEffect()
-        }
-    }, [])
+  const handleJumpClick = () => {
+    setIsJumping(true);
+  };
 
-    return <div className="interface">
+  const handleControlButtonRelease = () => {
+    setIsMovingForward(false);
+    setIsMovingBackward(false);
+    setIsMovingLeftward(false);
+    setIsMovingRightward(false);
+    setIsJumping(false);
+  };
 
-        {/* Time */}
-        <div ref={ time } className="time">0.00</div>
+  useEffect(() => {
+    const unsubscribeEffect = addEffect(() => {
+      const state = useGame.getState();
 
-        {/* Restart */}
-        { phase === 'ended' && <div className="restart" onClick={ restart }>Restart</div> }
+      let elapsedTime = 0;
 
-        {/* Controls */}
-        <div className="controls">
-            <div className="raw">
-                <div className={ `key ${ forward ? 'active' : '' }` }></div>
-            </div>
-            <div className="raw">
-                <div className={ `key ${ leftward ? 'active' : '' }` }></div>
-                <div className={ `key ${ backward ? 'active' : '' }` }></div>
-                <div className={ `key ${ rightward ? 'active' : '' }` }></div>
-            </div>
-            <div className="raw">
-                <div className={ `key large ${ jump ? 'active' : '' }` }></div>
-            </div>
+      if (state.phase === 'playing') elapsedTime = Date.now() - state.startTime;
+      else if (state.phase === 'ended') elapsedTime = state.endTime - state.startTime;
+
+      elapsedTime /= 1000;
+      elapsedTime = elapsedTime.toFixed(2);
+
+      if (time.current) time.current.textContent = elapsedTime;
+    });
+
+    return () => {
+      unsubscribeEffect();
+    };
+  }, []);
+
+  useEffect(() => {
+    // Your game logic using the movement states
+    // For example:
+    if (isMovingForward) {
+      // Move player forward
+    } else if (isMovingBackward) {
+      // Move player backward
+    } else if (isMovingLeftward) {
+      // Move player leftward
+    } else if (isMovingRightward) {
+      // Move player rightward
+    }
+
+    if (isJumping) {
+      // Perform player jump
+    }
+  }, [
+    isMovingForward,
+    isMovingBackward,
+    isMovingLeftward,
+    isMovingRightward,
+    isJumping,
+  ]);
+
+  return (
+    <div className="interface">
+      {/* Time */}
+      <div ref={time} className="time">
+        0.00
+      </div>
+
+      {/* Restart */}
+      {phase === 'ended' && (
+        <div className="restart" onClick={restart}>
+          Restart
         </div>
-        
+      )}
+
+      {/* Controls */}
+      <div className="controls">
+        <div className="raw">
+          <div
+            className={`key ${isMovingForward ? 'active' : ''}`}
+            onClick={handleForwardClick}
+            onMouseUp={handleControlButtonRelease}
+            onMouseLeave={handleControlButtonRelease}
+          ></div>
+        </div>
+        <div className="raw">
+          <div
+            className={`key ${isMovingLeftward ? 'active' : ''}`}
+            onClick={handleLeftwardClick}
+            onMouseUp={handleControlButtonRelease}
+            onMouseLeave={handleControlButtonRelease}
+          ></div>
+          <div
+            className={`key ${isMovingBackward ? 'active' : ''}`}
+            onClick={handleBackwardClick}
+            onMouseUp={handleControlButtonRelease}
+            onMouseLeave={handleControlButtonRelease}
+          ></div>
+          <div
+            className={`key ${isMovingRightward ? 'active' : ''}`}
+            onClick={handleRightwardClick}
+            onMouseUp={handleControlButtonRelease}
+            onMouseLeave={handleControlButtonRelease}
+          ></div>
+        </div>
+        <div className="raw">
+          <div
+            className={`key large ${isJumping ? 'active' : ''}`}
+            onClick={handleJumpClick}
+            onMouseUp={handleControlButtonRelease}
+            onMouseLeave={handleControlButtonRelease}
+          ></div>
+        </div>
+      </div>
     </div>
+  );
 }
