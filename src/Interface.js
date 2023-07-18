fe8d887c2dfadd03f1ab3b31f5680d11c133f035
imrobbiegreen <66812611+imrobbiegreen@users.noreply.github.com>
2023-07-18 13:55:55 -0500

    Update Interface.js

--- src/Interface.js
+++ src/Interface.js
@@ -1,6 +1,6 @@
 import { useKeyboardControls } from '@react-three/drei'
 import useGame from './stores/useGame.js'
-import { useState, useEffect, useRef } from 'react'
+import { useEffect, useRef } from 'react'
 import { addEffect } from '@react-three/fiber'
 
 export default function Interface()
@@ -10,40 +10,11 @@ export default function Interface()
     const restart = useGame((state) => state.restart)
     const phase = useGame((state) => state.phase)
 
-    const keyboardForward = useKeyboardControls((state) => state.forward)
-    const keyboardBackward = useKeyboardControls((state) => state.backward)
-    const keyboardLeftward = useKeyboardControls((state) => state.leftward)
-    const keyboardRightward = useKeyboardControls((state) => state.rightward)
-    const keyboardJump = useKeyboardControls((state) => state.jump)
-
-    const [deviceForward, setDeviceForward] = useState(false)
-    const [deviceBackward, setDeviceBackward] = useState(false)
-    const [deviceLeftward, setDeviceLeftward] = useState(false)
-    const [deviceRightward, setDeviceRightward] = useState(false)
-
-    useEffect(() => {
-        const handleOrientation = (event) => {
-            var x = event.beta  
-            var y = event.gamma 
-            
-            if(x > 10) setDeviceForward(true)
-            else if(x < -10) setDeviceBackward(true)
-            else {
-                setDeviceForward(false)
-                setDeviceBackward(false)
-            }
-
-            if(y > 10) setDeviceRightward(true)
-            else if(y < -10) setDeviceLeftward(true)
-            else {
-                setDeviceRightward(false)
-                setDeviceLeftward(false)
-            }
-        }
-
-        window.addEventListener('deviceorientation', handleOrientation, true)
-        return () => window.removeEventListener('deviceorientation', handleOrientation)
-    }, [])
+    const forward = useKeyboardControls((state) => state.forward)
+    const backward = useKeyboardControls((state) => state.backward)
+    const leftward = useKeyboardControls((state) => state.leftward)
+    const rightward = useKeyboardControls((state) => state.rightward)
+    const jump = useKeyboardControls((state) => state.jump)
 
     useEffect(() =>
     {
@@ -82,15 +53,15 @@ export default function Interface()
         {/* Controls */}
         <div className="controls">
             <div className="raw">
-                <div className={ `key ${ keyboardForward || deviceForward ? 'active' : '' }` }></div>
+                <div className={ `key ${ forward ? 'active' : '' }` }></div>
             </div>
             <div className="raw">
-                <div className={ `key ${ keyboardLeftward || deviceLeftward ? 'active' : '' }` }></div>
-                <div className={ `key ${ keyboardBackward || deviceBackward ? 'active' : '' }` }></div>
-                <div className={ `key ${ keyboardRightward || deviceRightward ? 'active' : '' }` }></div>
+                <div className={ `key ${ leftward ? 'active' : '' }` }></div>
+                <div className={ `key ${ backward ? 'active' : '' }` }></div>
+                <div className={ `key ${ rightward ? 'active' : '' }` }></div>
             </div>
             <div className="raw">
-                <div className={ `key large ${ keyboardJump ? 'active' : '' }` }></div>
+                <div className={ `key large ${ jump ? 'active' : '' }` }></div>
             </div>
         </div>
         
